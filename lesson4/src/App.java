adacf9b4133622f7f49bacb7ada0ae096c4f9f58
khanh <baokhanh.gbli@gmail.com>
2021-10-20 09:37:52 +0700

    change some code

--- lesson4/src/App.java
+++ lesson4/src/App.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        chooseHomework();
-    }
-
-    public static void chooseHomework() {
         System.out.println("hãy chọn 1 bài tập: ");
         System.out.println("1 - kiểm tra 3 cạnh có tạo thành 1 tam giác");
         System.out.println("2 - tính điểm TBC");
         System.out.println("3 - tính phuong trình bậc 2");
-        Scanner scanner = new Scanner(System.in);
         System.out.print("bạn chọn bài tập: ");
+        menu();
+    }
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
         int chooseHomework = scanner.nextInt();
         switch (chooseHomework) {
             case 1:
--- lesson4/src/QuadraticEquation.java
+++ lesson4/src/QuadraticEquation.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 public class QuadraticEquation {
     private static int a;
     private static int b;
     private static int c;
 
-    public static void enterInput(){
+    public static void enterInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("phương trình bậc 2 có dạng ax^2 + bx + c = 0 với a != 0");
         System.out.println("nhập 3 hệ số: ");
@@ -14,20 +15,25 @@ public class QuadraticEquation {
         b = scanner.nextInt();
         System.out.print("c = ");
         c = scanner.nextInt();
-        findRoot();
+
+        if (a == 0) {
+            System.out.println("a không phù hợp với điều kiên do a = 0");
+        } else
+            findRoot();
+
         scanner.close();
     }
 
-    public static void findRoot(){
+    public static void findRoot() {
         double delta = calcDelta();
-        if (delta < 0){
+        if (delta < 0) {
             System.out.println("phương trình vô nghiệm");
-        } else if (delta == 0){
-            double x = -b/(2*a);
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
             System.out.printf("Phương trình có nghiệm kép: x = %.2f", x);
         } else {
-            double x1 = (-b + Math.sqrt(delta))/(2*a);
-            double x2 = (-b - Math.sqrt(delta))/(2*a);
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
             System.out.println("Phương trình có 2 nghiệm: ");
             System.out.printf("x1 = %.2f\n", x1);
             System.out.printf("x2 = %.2f\n", x2);
@@ -35,8 +41,8 @@ public class QuadraticEquation {
 
     }
 
-    public static double calcDelta(){
-        return (double) Math.pow(b,2) - 4 * a * c;
+    public static double calcDelta() {
+        return (double) Math.pow(b, 2) - 4 * a * c;
     }
 
 }
