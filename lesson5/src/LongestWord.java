94f89e93769c9180c2d07776815cac2b04ae69e3
khanh <baokhanh.gbli@gmail.com>
2021-10-22 23:25:02 +0700

    change some code

--- lesson5/src/LongestWord.java
+++ lesson5/src/LongestWord.java
@@ -4,7 +4,7 @@ public class LongestWord {
         String longestWord = "";
         for (int i = 0; i < input.length(); i++) {
             if(input.charAt(i) == ' '){
-                if(currentWord.length() > longestWord.length()){
+                if(currentWord.length() >= longestWord.length()){
                     longestWord = currentWord;
                 }
                 currentWord = "";
