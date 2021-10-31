4e2cc39405aaabc5f736f269a27e2706a982894f
khanh <baokhanh.gbli@gmail.com>
2021-11-01 02:34:14 +0700

    homework lesson 8

---
+++ lesson8/homework/src/App.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Student[] students = new Student[3];
+        getInput(students);
+        show(students);
+    }
+
+    public static void getInput(Student[] students){
+        System.out.println("nhập thông tin");
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < students.length; i++){
+            students[i] = getStudent(scanner);
+            System.out.println("--------------------------------");
+        }
+
+        scanner.close();
+    }
+
+    public static void show(Student[] students){
+        System.out.println("in thong tin: ");
+        for (Student student : students) {
+            System.out.println(student.toString());
+            System.out.println("--------------------------------");
+        } 
+    }
+
+    public static Student getStudent(Scanner scanner){
+        System.out.print("id: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("\ntên: ");
+        String name = scanner.nextLine();
+        System.out.print("\nđiểm lý thuyết: ");
+        int theoreticalScore = scanner.nextInt();
+        System.out.print("\nđiểm thực hành: ");
+        int practicalScore = scanner.nextInt();
+        Student student = new Student(id, name, theoreticalScore, practicalScore);
+        return student;
+    }
+}
+
---
+++ lesson8/homework/src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    int id;
+    String name;
+    int theoreticalScore;
+    int practicalScore;
+    
+    public Student(int id, String name, int theoreticalScore, int practicalScore){
+        this.id = id;
+        this.name = name;
+        this. theoreticalScore = theoreticalScore;
+        this.practicalScore = practicalScore;
+    }
+    
+    @Override
+    public String toString() {
+        return "id: " + id 
+        + "\ntên: " + name 
+        + "\nđiểm lý thuyết: " + theoreticalScore 
+        + "\nđiểm thực hành: " + practicalScore 
+        + "\nđiểm trung bình: " + getGPA();
+    }
+
+    public double getGPA() {
+        return (theoreticalScore + practicalScore) / 2;
+    }
+}
+
