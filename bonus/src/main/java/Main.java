19621147d29a841303b09181eef93d50d062c8fe
khanh <baokhanh.gbli@gmail.com>
2021-11-18 01:29:20 +0700

    finish bonus excercise

--- bonus/src/main/java/Main.java
+++ bonus/src/main/java/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args){
         PlayerRepository.getData();
         PlayerRepository.print();
+
+        System.out.println("lựa chọn 11 cầu thủ");
+        System.out.println("1GK - 4DF - 4MF - 2FW");
+        PlayerRepository.getTeams(4, 4, 2);
+        System.out.println("-----------------------------");
+        System.out.println("1GK - 4DF - 3MF - 3FW");
+        PlayerRepository.getTeams(4, 3, 3);
+        System.out.println("-----------------------------");
+        System.out.println("1GK - 3DF - 5MF - 2FW");
+        PlayerRepository.getTeams(3, 5, 2);
     }
 }
--- bonus/src/main/java/PlayerRepository.java
+++ bonus/src/main/java/PlayerRepository.java
@@ -1,46 +1,104 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PlayerRepository {
-    private static ArrayList<Player> playerArrayList = new ArrayList<>();
+    private static ArrayList<Player> allPlayers = new ArrayList<>();
+    private static ArrayList<Player> allGoalKeeper = new ArrayList<>();
+    private static ArrayList<Player> allMidField = new ArrayList<>();
+    private static ArrayList<Player> allForward = new ArrayList<>();
+    private static ArrayList<Player> allDefender = new ArrayList<>();
 
-    public static void getData(){
-        playerArrayList.add(new Player(1, "bùi tấn trường", Position.GK));
-        playerArrayList.add(new Player(23, "trần mạnh nguyên", Position.GK));
-        playerArrayList.add(new Player(12, "nguyễn văn hoàng", Position.GK));
-        playerArrayList.add(new Player(13, "hồ tấn tài", Position.DF));
-        playerArrayList.add(new Player(16, "nguyễn thành chung", Position.DF));
-        playerArrayList.add(new Player(2, "đỗ duy mạnh", Position.DF));
-        playerArrayList.add(new Player(7, "nguyễn phong hồng duy", Position.DF));
-        playerArrayList.add(new Player(17, "vũ văn thanh", Position.DF));
-        playerArrayList.add(new Player(5, "phạm xuân mạnh", Position.DF));
-        playerArrayList.add(new Player(3, "quế ngọc hải", Position.DF));
-        playerArrayList.add(new Player(4, "bùi tiến dũng", Position.DF));
-        playerArrayList.add(new Player(21, "bùi hoàng việt anh", Position.DF));
-        playerArrayList.add(new Player(19, "nguyễn quang hải", Position.MF));
-        playerArrayList.add(new Player(15, "phạm đức huy", Position.MF));
-        playerArrayList.add(new Player(6, "lương xuân trường", Position.MF));
-        playerArrayList.add(new Player(20, "phan văn đức", Position.MF));
-        playerArrayList.add(new Player(14, "nguyễn hoàng đức", Position.MF));
-        playerArrayList.add(new Player(8, "lê văn xuân", Position.MF));
-        playerArrayList.add(new Player(11, "nguyễn tuấn anh", Position.MF));
-        playerArrayList.add(new Player(9, "nguyễn văn toàn", Position.FW));
-        playerArrayList.add(new Player(22, "nguyễn tiến linh", Position.FW));
-        playerArrayList.add(new Player(10, "nguyễn công phượng", Position.FW));
-        playerArrayList.add(new Player(18, "hà đức chính", Position.FW));
+    public static void getData() {
+        allPlayers.add(new Player(1, "bùi tấn trường", Position.GK));
+        allPlayers.add(new Player(23, "trần mạnh nguyên", Position.GK));
+        allPlayers.add(new Player(12, "nguyễn văn hoàng", Position.GK));
+        allPlayers.add(new Player(13, "hồ tấn tài", Position.DF));
+        allPlayers.add(new Player(16, "nguyễn thành chung", Position.DF));
+        allPlayers.add(new Player(2, "đỗ duy mạnh", Position.DF));
+        allPlayers.add(new Player(7, "nguyễn phong hồng duy", Position.DF));
+        allPlayers.add(new Player(17, "vũ văn thanh", Position.DF));
+        allPlayers.add(new Player(5, "phạm xuân mạnh", Position.DF));
+        allPlayers.add(new Player(3, "quế ngọc hải", Position.DF));
+        allPlayers.add(new Player(4, "bùi tiến dũng", Position.DF));
+        allPlayers.add(new Player(21, "bùi hoàng việt anh", Position.DF));
+        allPlayers.add(new Player(19, "nguyễn quang hải", Position.MF));
+        allPlayers.add(new Player(15, "phạm đức huy", Position.MF));
+        allPlayers.add(new Player(6, "lương xuân trường", Position.MF));
+        allPlayers.add(new Player(20, "phan văn đức", Position.MF));
+        allPlayers.add(new Player(14, "nguyễn hoàng đức", Position.MF));
+        allPlayers.add(new Player(8, "lê văn xuân", Position.MF));
+        allPlayers.add(new Player(11, "nguyễn tuấn anh", Position.MF));
+        allPlayers.add(new Player(9, "nguyễn văn toàn", Position.FW));
+        allPlayers.add(new Player(22, "nguyễn tiến linh", Position.FW));
+        allPlayers.add(new Player(10, "nguyễn công phượng", Position.FW));
+        allPlayers.add(new Player(18, "hà đức chính", Position.FW));
     }
 
-    public static ArrayList<Player> getAllPlayers(){
-        return playerArrayList;
+    public static ArrayList<Player> getAllPlayers() {
+        return allPlayers;
     }
 
-    public  static  void print(){
-        for (Player player: playerArrayList
-             ) {
+    public static void print() {
+        for (Player player : allPlayers
+        ) {
             System.out.println(player.toString());
         }
     }
 
-    public static void getTeams(){
 
+    public static void getTeams(int DFsize, int MFsize, int FWsize) {
+        ArrayList<Player> selectedTeam = new ArrayList<Player>();
+        Random general = new Random();
+        getPositions();
+
+        int defenderSize = 0;
+        int midFieldSize = 0;
+        int forwardSize = 0;
+
+        int rdGoalKeeper = general.nextInt(allGoalKeeper.size());
+        selectedTeam.add(allGoalKeeper.get(rdGoalKeeper));
+
+        while (defenderSize < DFsize) {
+            int rdDefender = general.nextInt(allDefender.size());
+            if (!selectedTeam.contains(allDefender.get(rdDefender))) {
+                selectedTeam.add(allDefender.get(rdDefender));
+                defenderSize++;
+            }
+        }
+
+        while (midFieldSize < MFsize) {
+            int rdMidField = general.nextInt(allMidField.size());
+            if (!selectedTeam.contains(allMidField.get(rdMidField))) {
+                selectedTeam.add(allMidField.get(rdMidField));
+                midFieldSize++;
+            }
+        }
+
+        while (forwardSize < FWsize) {
+            int rdForward = general.nextInt(allForward.size());
+            if (!selectedTeam.contains(allForward.get(rdForward))) {
+                selectedTeam.add(allForward.get(rdForward));
+                forwardSize++;
+            }
+        }
+
+        for (Player player : selectedTeam
+        ) {
+            System.out.println(player.toString());
+        }
+    }
+
+    public static void getPositions() {
+        for (Player player : allPlayers
+        ) {
+            if (player.getPosition().equals(Position.GK)) {
+                allGoalKeeper.add(player);
+            } else if (player.getPosition().equals(Position.FW)) {
+                allForward.add(player);
+            } else if (player.getPosition().equals(Position.MF)) {
+                allMidField.add(player);
+            } else allDefender.add(player);
+
+        }
     }
 }
